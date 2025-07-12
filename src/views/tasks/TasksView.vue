edd289bf0415310ed4f3257355249b8eb306a33a
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-07-12 15:49:51 +0200

    chore: add list of example tasks in tasksView with the add button

--- src/views/tasks/TasksView.vue
+++ src/views/tasks/TasksView.vue
@@ -1,6 +1,88 @@
 <template>
-  <div>
-    <h1>Tasks</h1>
+  <ul class="list rounded-box">
+    <li class="list-row">
+      <div class="list-col-grow flex items-center">
+        <div>Relaxing Lights</div>
+      </div>
+      <button class="btn btn-circle btn-ghost">
+        <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M6 3L20 12 6 21 6 3z"></path>
+          </g>
+        </svg>
+      </button>
+    </li>
+
+    <li class="list-row">
+      <div class="list-col-grow flex items-center">
+        <div>Start Roomba</div>
+      </div>
+      <button class="btn btn-circle btn-ghost">
+        <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M6 3L20 12 6 21 6 3z"></path>
+          </g>
+        </svg>
+      </button>
+    </li>
+
+    <li class="list-row">
+      <div class="list-col-grow flex items-center">
+        <div>Close windows</div>
+      </div>
+      <button class="btn btn-circle btn-ghost">
+        <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M6 3L20 12 6 21 6 3z"></path>
+          </g>
+        </svg>
+      </button>
+    </li>
+
+    <li class="list-row">
+      <div class="list-col-grow flex items-center">
+        <div>Play music (Emma's room)</div>
+      </div>
+      <button class="btn btn-circle btn-ghost">
+        <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M6 3L20 12 6 21 6 3z"></path>
+          </g>
+        </svg>
+      </button>
+    </li>
+  </ul>
+
+  <div class="flex justify-end px-4">
+    <button class="btn btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" class="size-[1.2em]" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+      </svg>
+    </button>
   </div>
 </template>
 
