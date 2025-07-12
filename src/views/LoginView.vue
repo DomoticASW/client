854f6b13bf9ce830b25a77537c81579bbc3d793a
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-07-12 19:34:37 +0200

    chore: add basic login and registration views

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,47 @@
 <template>
   <div>
-    <h1>Login</h1>
+    <h1>Welcome back!</h1>
+    
+    <form @submit.prevent="handleLogin">
+      <div>
+        <label>Server address</label>
+        <input v-model="serverAddress" type="text" required>
+      </div>
+      
+      <div>
+        <label>Email</label>
+        <input v-model="email" type="email" required>
+      </div>
+      
+      <div>
+        <label>Password</label>
+        <input v-model="password" type="password" required>
+      </div>
+      
+      <button type="submit">Log in</button>
+    </form>
+    
+    <p>Do you need an account?</p>
+    <button @click="goToSignUp">Sign up</button>
   </div>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const serverAddress = ref('')
+const email = ref('')
+const password = ref('')
+
+const router = useRouter()
+
+function handleLogin() {
+  console.log('Login submitted');
+}
+
+function goToSignUp() {
+  router.push('/register');
+}
+</script>
 
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,54 @@
 <template>
   <div>
-    <h1>Register</h1>
+    <h1>Welcome!</h1>
+    
+    <form @submit.prevent="handleSignUp">
+      <div>
+        <label>Server address</label>
+        <input v-model="serverAddress" type="text" required>
+      </div>
+      
+      <div>
+        <label>Nickname</label>
+        <input v-model="nickname" type="text" required>
+      </div>
+      
+      <div>
+        <label>Email</label>
+        <input v-model="email" type="email" required>
+      </div>
+      
+      <div>
+        <label>Password</label>
+        <input v-model="password" type="password" required>
+      </div>
+      
+      <p>You will need to wait for the administrator to accept your request before log in!</p>
+      
+      <button type="submit">Sign up</button>
+    </form>
+      
+    <p>Do you have already an account?</p>
+    <button @click="goToLogin">Log in</button>
   </div>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const serverAddress = ref('');
+const nickname = ref('');
+const email = ref('');
+const password = ref('');
+
+const router = useRouter();
+
+function handleSignUp() {
+  console.log('Sign up submitted');
+}
+
+function goToLogin() {
+  router.push('/login');
+}
+</script>
