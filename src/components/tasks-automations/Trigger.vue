922387f24a989fab421d00935a7d301cb67d834d
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-07-19 16:41:51 +0200

    chore: make Trigger a component and add buttons to create-automation page

---
+++ src/components/tasks-automations/Trigger.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { formatDate, formatDuration, trigger } from '@/components/tasks-automations/example';
+</script>
+
+<template>
+  <div class="card card-sm bg-secondary/70 text-secondary-content my-2" v-if="$route.params.id !== undefined">
+    <div class="card-body text-base grid grid-cols-2 px-4">
+      <p>Start</p>
+      <p class="font-bold justify-self-center truncate">{{ formatDate(trigger.start) }}</p>
+
+      <p>Repeat every</p>
+      <p class="justify-self-center truncate">{{ formatDuration(trigger.periodSeconds) }} </p>
+    </div>
+  </div>
+</template>
--- src/views/automations/EditAutomationView.vue
+++ src/views/automations/EditAutomationView.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
   import InstructionReorder from '@/components/tasks-automations/InstructionReorder.vue'
   import { instructions } from '@/components/tasks-automations/example';
+  import Trigger from '@/components/tasks-automations/Trigger.vue';
 </script>
 
 <template>
   <div class="mx-6">
     <!-- TODO: put name and not id (finding the automation from the server) -->
-    <input type="text" placeholder="Automation name" class="input w-full" :value="$route.params.id != undefined ? $route.params.id : ''"/>
+    <input type="text" placeholder="Automation name" class="input w-full" :value="$route.params.id !== undefined ? $route.params.id : ''"/>
   </div>
   <hr class="m-4" />
+  <h1 class="text-xl">Trigger</h1>
+  <div class="grid grid-cols-2 p-4 justify-items-center" v-if="$route.params.id === undefined">
+    <button class="btn btn-neutral w-50" type="button">Device Event</button>
+    <button class="btn btn-neutral w-50" type="button">Period</button>
+  </div>
+  <Trigger v-if="$route.params.id !== undefined"/>
+  <hr class="m-4">
+  <h1 class="text-xl">Actions</h1>
   <!-- instructions need to be taken from the server when loading the page, if adding a new automation it is not needed (v-if already doing it) -->
-  <InstructionReorder :instructions="instructions" v-if="$route.params.id != undefined"/>
+  <InstructionReorder :instructions="instructions" v-if="$route.params.id !== undefined"/>
   <div class="pb-4"></div>
 </template>
