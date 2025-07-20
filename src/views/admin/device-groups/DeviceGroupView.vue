259506fd5f03104587f85bc091070c8f11897690
Alessandro Venturini <ales.ventus21@gmail.com>
2025-07-20 18:24:18 +0200

    feat: fetching DeviceGroup and add/remove devices

--- src/views/admin/device-groups/DeviceGroupView.vue
+++ src/views/admin/device-groups/DeviceGroupView.vue
@@ -4,41 +4,25 @@ import { computed, ref } from 'vue'
 import DeviceRowSkeleton from '@/components/admin/manage-devices/DeviceListSkeleton.vue'
 import type { DeviceGroup } from '@/model/devices-management/DeviceGroup'
 import type { Device } from '@/model/devices-management/Device'
+import { authorizedRequest } from '@/utils'
+import { useLoadingOverlayStore } from '@/stores/loading-overlay'
+import { useUserInfoStore } from '@/stores/user-info'
 
 const props = defineProps({ id: { type: String, required: true } })
+const userInfo = useUserInfoStore()
+const loadingOverlay = useLoadingOverlayStore()
 
 const group = ref<DeviceGroup | undefined>(undefined)
-// TODO: call the sever
-setTimeout(
-  () =>
-    (group.value = {
-      id: props.id,
-      name: 'Kitchen',
-      devices: [
-        { id: '1', name: 'Roomba' },
-        { id: '2', name: 'Thermometer' },
-        { id: '3', name: 'Lamp' },
-      ],
-    }),
-  1000,
-)
+authorizedRequest(`/api/deviceGroups/${props.id}`, userInfo.token)
+  .then(({ json }) => (group.value = json as DeviceGroup))
+  // TODO: present error to the user
+  .catch((e) => console.log(e))
 
 const devices = ref<Device[] | undefined>(undefined)
-setTimeout(
-  () =>
-    (devices.value = [
-      { id: '1', name: 'Roomba' },
-      { id: '2', name: 'Thermometer' },
-      { id: '3', name: 'Lamp' },
-      { id: '4', name: 'Lamp2' },
-      { id: '5', name: 'Lamp3' },
-      { id: '6', name: 'Lamp4' },
-      { id: '7', name: 'Lamp5' },
-      { id: '8', name: 'Lamp6' },
-      { id: '9', name: 'Lamp7' },
-    ]),
-  1500,
-)
+authorizedRequest(`/api/devices`, userInfo.token)
+  .then(({ json }) => (devices.value = json as Device[]))
+  // TODO: present error to the user
+  .catch((e) => console.log(e))
 
 const devicesNotInGroup = computed(() => {
   if (group.value && devices.value)
@@ -48,9 +32,22 @@ const devicesNotInGroup = computed(() => {
   else return undefined
 })
 function removeDeviceFromGroup(deviceId: string) {
+  authorizedRequest(`/api/deviceGroups/${props.id}/device/${deviceId}`, userInfo.token, {
+    method: 'DELETE',
+  })
+    // TODO: present error to the user
+    .catch((e) => console.log(e))
+  // TODO: loading
   group.value!.devices = group.value!.devices.filter((d) => d.id != deviceId)
 }
 function addDeviceToGroup(deviceId: string) {
+  authorizedRequest(`/api/deviceGroups/${props.id}/device`, userInfo.token, {
+    method: 'POST',
+    body: JSON.stringify({ deviceId }),
+  })
+    // TODO: present error to the user
+    .catch((e) => console.log(e))
+  // TODO: loading
   const device = devices.value!.find((d) => d.id == deviceId)
   if (device) {
     group.value!.devices.push(device)
