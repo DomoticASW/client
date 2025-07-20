96247cdb38da5a5fd6bacedfb58d7981b5cc8d84
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-07-20 17:16:15 +0200

    chore: link automations and tasks permissions to server logic

--- src/views/admin/automations-task-permissions/AutomationsAndTasksPermissionsView.vue
+++ src/views/admin/automations-task-permissions/AutomationsAndTasksPermissionsView.vue
@@ -1,6 +1,34 @@
 <script setup lang="ts">
-const tasks = ['Turns lights off', 'Close windows', "Play music (Emma's room)"]
-const automations = ['Washing machine done']
+import { useUserInfoStore } from '@/stores/user-info'
+import { authorizedRequest } from '@/utils'
+import { ref } from 'vue'
+
+interface Task {
+  readonly id: string
+  name: string
+
+  instructions: any[]
+}
+
+interface Automation {
+  readonly id: string
+  name: string
+
+  instructions: any[]
+
+  enabled: boolean
+  trigger: any
+}
+
+const userInfo = useUserInfoStore()
+const tasks = ref<Task[] | undefined>(undefined)
+const automations = ref<Automation[] | undefined>(undefined)
+authorizedRequest('/api/tasks', userInfo.token).then(({ json }) => {
+  tasks.value = json as Task[]
+})
+authorizedRequest('/api/automations', userInfo.token).then(({ json }) => {
+  automations.value = json as Automation[]
+})
 </script>
 
 <template>
@@ -9,15 +37,15 @@ const automations = ['Washing machine done']
       <h1 class="text-2xl font-bold dark:text-white">Tasks</h1>
       <div>
         <ul class="list rounded-box">
-          <li class="list-row" v-for="task in tasks" :key="task">
+          <li class="list-row" v-for="task in tasks" :key="task.id">
             <div class="list-col-grow flex items-center">
-              {{ task }}
+              {{ task.name }}
             </div>
-            <RouterLink :to="{ name: 'automation-and-task-permission', params: { id: task } }">
+            <RouterLink :to="{ name: 'automation-and-task-permission', params: { id: task.id } }">
               <button
                 class="btn btn-circle btn-ghost"
                 type="button"
-                :aria-label="'Get permissions of: ' + task"
+                :aria-label="'Get permissions of: ' + task.name"
               >
                 <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <g
@@ -41,17 +69,17 @@ const automations = ['Washing machine done']
       <h1 class="text-2xl font-bold dark:text-white">Automations</h1>
       <div>
         <ul class="list rounded-box">
-          <li class="list-row" v-for="automation in automations" :key="automation">
+          <li class="list-row" v-for="automation in automations" :key="automation.id">
             <div class="list-col-grow flex items-center">
-              {{ automation }}
+              {{ automation.name }}
             </div>
             <RouterLink
-              :to="{ name: 'automation-and-task-permission', params: { id: automation } }"
+              :to="{ name: 'automation-and-task-permission', params: { id: automation.id } }"
             >
               <button
                 class="btn btn-circle btn-ghost"
                 type="button"
-                :aria-label="'Get permissions of: ' + automation"
+                :aria-label="'Get permissions of: ' + automation.name"
               >
                 <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <g
