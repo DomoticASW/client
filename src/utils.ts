8aa6a290d5f0b9e4962e47178e41d9f5228308a9
Alessandro Venturini <ales.ventus21@gmail.com>
2025-07-20 11:32:20 +0200

    fix: error when deserializing empty body

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,8 @@
  * Sets the Authorization header to token if it is not already set.
  * Sets the Content-Type header to "application/json" if it is not already set.
  */
-export async function authorizedRequest(input: RequestInfo | URL, token: string, init?: RequestInit): Promise<{ response: Response, json: object }> {
-  const headers = new Headers(init?.headers) // Enuring that the type of init.headers is Headers
+export async function authorizedRequest(input: RequestInfo | URL, token: string, init?: RequestInit): Promise<{ response: Response, json: object | undefined }> {
+  const headers = new Headers(init?.headers) // Ensuring that the type of init.headers is Headers
   if (!headers.has("Authorization")) {
     headers.append("Authorization", token)
   }
@@ -13,11 +13,23 @@ export async function authorizedRequest(input: RequestInfo | URL, token: string,
 /**
  * Sets the Content-Type header to "application/json" if it is not already set.
  */
-export async function request(input: RequestInfo | URL, init?: RequestInit): Promise<{ response: Response, json: object }> {
-  const headers = new Headers(init?.headers) // Enuring that the type of init.headers is Headers
+export async function request(input: RequestInfo | URL, init?: RequestInit): Promise<{ response: Response, json: object | undefined }> {
+  const headers = new Headers(init?.headers) // Ensuring that the type of init.headers is Headers
   if (!headers.has("Content-Type")) {
     headers.append("Content-Type", "application/json")
   }
-  const response = await fetch(input, init)
-  return { response, json: await response.json() }
+  const response = await fetch(input, { ...init, headers })
+  // Passing through a text representation as JSON.parse will fail in case of empty body
+  const bodyAsText = await response.text()
+  const body: object | undefined = bodyAsText ? JSON.parse(bodyAsText) : undefined
+  if (!response.ok) {
+    throw (body as ServerError)
+  }
+  return { response, json: body }
+}
+
+export interface ServerError {
+  __brand: string
+  message: string
+  cause?: string
 }
