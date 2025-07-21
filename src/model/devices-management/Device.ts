1364492758ce21e56e279ede1dd3107133d903ea
Alessandro Venturini <ales.ventus21@gmail.com>
2025-07-21 18:36:59 +0200

    chore: updated model

--- src/model/devices-management/Device.ts
+++ src/model/devices-management/Device.ts
@@ -1,5 +1,10 @@
-export interface Device{
+export interface Device {
   id: string
   name: string
+  address: DeviceAddress
 }
 
+export interface DeviceAddress {
+  host: string
+  port: number
+}
