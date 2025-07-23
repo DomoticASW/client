9a7d91a7f075a92e059b5e86d4576f998b509bde
Alessandro Venturini <ales.ventus21@gmail.com>
2025-07-23 11:45:57 +0200

    fix: subsequent errors visual bugs

--- src/components/ErrorPresenter.vue
+++ src/components/ErrorPresenter.vue
@@ -2,13 +2,17 @@
 import { useErrorPresenterStore } from '@/stores/error-presenter'
 import { computed, onMounted, ref } from 'vue'
 
+/** Approximate duration of the dialog closing animation.
+ * It's needed in order to correctly show fast subsequent errors.
+ */
+const dialogClosingAnimationDurationMs = 200
 const state = useErrorPresenterStore()
 
 const dialogId = 'error-presenter-dialog'
 const dialog = ref<HTMLDialogElement | undefined>()
 
 const errorTitle = computed(() => {
-  const error = state.errorShown ?? state.lastShownError
+  const error = state.errorShown
   if (error && '__brand' in error) {
     return error.__brand as string
   } else {
@@ -16,19 +20,23 @@ const errorTitle = computed(() => {
   }
 })
 const errorMessage = computed(() => {
-  const error = state.errorShown ?? state.lastShownError
+  const error = state.errorShown
   if (error && 'message' in error) {
     return error.message as string
   } else {
-    return undefined
+    return ''
   }
 })
+
+async function okPressed() {
+  dialog.value!.close()
+  await new Promise((r) => setTimeout(r, dialogClosingAnimationDurationMs))
+  state.errorWasViewed()
+}
 
 state.$subscribe(async () => {
   if (state.errorShown) {
     dialog.value!.showModal()
-  } else {
-    dialog.value!.close()
   }
 })
 onMounted(() => {
@@ -43,7 +51,7 @@ onMounted(() => {
       <h3 class="text-lg font-bold">{{ errorTitle }}</h3>
       <p>{{ errorMessage }}</p>
       <div class="modal-action">
-        <button class="btn btn-primary btn-soft" @click="state.stopShowingError()">Ok</button>
+        <button class="btn btn-primary btn-soft" @click="okPressed">Ok</button>
       </div>
     </div>
   </dialog>
