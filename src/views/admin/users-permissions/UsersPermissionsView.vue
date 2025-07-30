654a129cad84aa04eebfdba7ae2425e5da0be58a
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-07-30 10:32:08 +0200

    chore: add logic for UsersPermissionsView

--- src/views/admin/users-permissions/UsersPermissionsView.vue
+++ src/views/admin/users-permissions/UsersPermissionsView.vue
@@ -1,23 +1,14 @@
-<script setup lang="ts">
-import router from '@/router'
-
-const users = ['Mia', 'Alex', 'Emma']
-
-function goToPermissions(user: string) {
-  router.push(`./users-permissions/${user}`)
-}
-</script>
-
 <template>
+  <h1 class="text-3xl font-bold my-2">Manage user permissions</h1>
   <ul class="list rounded-box">
-    <li class="list-row" v-for="user in users" :key="user">
+    <li class="list-row" v-for="user in users" :key="user.email">
       <div class="list-col-grow flex items-center">
-        {{ user }}
+        {{ user.nickname }}
       </div>
       <button
         class="btn btn-circle btn-ghost"
         type="button"
-        :aria-label="'Get permissions of: ' + user"
+        :aria-label="'Get permissions of: ' + user.nickname"
         @click="goToPermissions(user)"
       >
         <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -36,4 +27,55 @@ function goToPermissions(user: string) {
   </ul>
 </template>
 
-<style></style>
+<script lang="ts">
+import router from '@/router';
+import { defineComponent, ref, onMounted } from 'vue';
+import { useUserInfoStore } from '@/stores/user-info';
+import { type User } from '@/model/users-management/User';
+
+export default defineComponent({
+  name: 'UsersPermissionsView',
+  setup() {
+    const users = ref<User[]>([]);
+
+    const loadUsers = async () => {
+      try {
+        const userInfoStore = useUserInfoStore();
+        userInfoStore.loadFromStorage();
+        const adminToken = userInfoStore.token;
+        
+        const getUsersResponse = await fetch('/api/users', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': adminToken
+          }
+        });
+
+        if (getUsersResponse.ok) {
+          const fetchedUsers = await getUsersResponse.json();
+          users.value = fetchedUsers;
+        } else {
+          console.error('Failed to fetch users:', getUsersResponse.status);
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      } finally {
+      }
+    };
+
+    onMounted(() => {
+      loadUsers();
+    });
+
+    return {
+      users
+    };
+  },
+  methods: {
+    goToPermissions(user: User): void {
+      router.push(`./users-permissions/${user.email}`)
+    }
+  }
+})
+</script>
