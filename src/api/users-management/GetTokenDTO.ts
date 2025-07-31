50e9962040d001f1c867a3f2572a403d3e3d4411
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-07-31 17:25:20 +0200

    chore: add tokenDTO

---
+++ src/api/users-management/GetTokenDTO.ts
@@ -0,0 +1,17 @@
+import { Deserializer } from "../Deserializer"
+
+export interface GetTokenDTO {
+  source: string;
+}
+
+export function isGetTokenDTO(o: unknown): o is GetTokenDTO {
+  return o != undefined && typeof o == "object" &&
+    "source" in o && typeof o.source == "string";
+}
+
+export const tokenDeserializer =
+  Deserializer<GetTokenDTO, string>(
+    isGetTokenDTO,
+    (dto) => dto.source,
+    (obj) => `Unable to deserialize ${obj} into a string since it was not a GetTokenDTO`
+  );
