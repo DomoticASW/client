42e75bfe55a11c51fd0d0ee07b582103ae88937c
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-08-03 14:00:44 +0200

    chore: refactor UsersPermissionsView to use Composition API

--- src/views/admin/users-permissions/UsersPermissionsView.vue
+++ src/views/admin/users-permissions/UsersPermissionsView.vue
@@ -27,50 +27,40 @@
   </ul>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import router from '@/router';
-import { defineComponent, ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useUserInfoStore } from '@/stores/user-info';
 import { type User } from '@/model/users-management/User';
 import * as api from '@/api/api';
 
-export default defineComponent({
-  name: 'UsersPermissionsView',
-  setup() {
-    const users = ref<User[]>([]);
+const users = ref<User[]>([]);
 
-    const loadUsers = async () => {
-        const userInfoStore = useUserInfoStore();
-        userInfoStore.loadFromStorage();
-        const adminToken = userInfoStore.token;
+const loadUsers = async () => {
+  const userInfoStore = useUserInfoStore();
+  userInfoStore.loadFromStorage();
+  const adminToken = userInfoStore.token;
 
-        const getUsersResponse = await api.authorizedRequest(
-          '/api/users',
-          adminToken,
-          { method: 'GET' }
-        );
+  const getUsersResponse = await api.authorizedRequest(
+    '/api/users',
+    adminToken,
+    { method: 'GET' }
+  );
 
-        users.value = await getUsersResponse.json();
-    };
+  users.value = await getUsersResponse.json();
+};
 
-    onMounted(() => {
-      loadUsers();
-    });
-
-    return {
-      users
-    };
-  },
-  methods: {
-    goToPermissions(user: User): void {
-      router.push({
-        path: `./${user.email}`,
-        state: { 
-          nickname: user.nickname,
-          email: user.email
-        }
-      })
+const goToPermissions = (user: User): void => {
+  router.push({
+    path: `./${user.email}`,
+    state: { 
+      nickname: user.nickname,
+      email: user.email
     }
-  }
-})
+  });
+};
+
+onMounted(() => {
+  loadUsers();
+});
 </script>
