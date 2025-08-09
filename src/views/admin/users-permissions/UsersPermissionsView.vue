58b91ef66ba2b5708131817908faa41cdd1296e2
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-08-09 21:14:31 +0200

    chore: refactor UsersPermissionsView to use navbar and show if there are no users

--- src/views/admin/users-permissions/UsersPermissionsView.vue
+++ src/views/admin/users-permissions/UsersPermissionsView.vue
@@ -1,30 +1,34 @@
 <template>
-  <h1 class="text-3xl font-bold my-2">Manage users permissions</h1>
-  <ul class="list rounded-box">
-    <li class="list-row" v-for="user in users" :key="user.email">
-      <div class="list-col-grow flex items-center">
-        {{ user.nickname }}
-      </div>
-      <button
-        class="btn btn-circle btn-ghost"
-        type="button"
-        :aria-label="'Get permissions of: ' + user.nickname"
-        @click="goToPermissions(user)"
-      >
-        <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <g
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            stroke-width="2"
-            fill="none"
-            stroke="currentColor"
-          >
-            <path d="M6 3L20 12 6 21 6 3z"></path>
-          </g>
-        </svg>
-      </button>
-    </li>
-  </ul>
+  <NavbarLayout title="Manage users permissions" :show-back-button="true">
+    <div v-if="users.length === 0" class="flex text-center text-gray-500 justify-center items-center min-h-[50vh]">
+      <p class="text-2xl">No users yet...</p>
+    </div>
+    <ul class="list rounded-box">
+      <li class="list-row" v-for="user in users" :key="user.email">
+        <div class="list-col-grow flex items-center">
+          {{ user.nickname }}
+        </div>
+        <button
+          class="btn btn-circle btn-ghost"
+          type="button"
+          :aria-label="'Get permissions of: ' + user.nickname"
+          @click="goToPermissions(user)"
+        >
+          <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              stroke-width="2"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M6 3L20 12 6 21 6 3z"></path>
+            </g>
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </NavbarLayout>
 </template>
 
 <script setup lang="ts">
@@ -33,13 +37,14 @@ import { ref, onMounted } from 'vue';
 import { useUserInfoStore } from '@/stores/user-info';
 import { Role, type User } from '@/model/users-management/User';
 import * as api from '@/api/users-management/requests/users';
+import NavbarLayout from '@/components/NavbarLayout.vue';
 
 const users = ref<User[]>([]);
 
 const loadUsers = async () => {
   const userInfoStore = useUserInfoStore();
   const adminToken = userInfoStore.token;
-
+  
   const usersList = await api.getAllUsers(adminToken);
   users.value = usersList.filter(user => user.role !== Role.Admin);
 };
