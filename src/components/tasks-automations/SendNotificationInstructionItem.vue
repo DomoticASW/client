33aefbbd20ccedfe3c535d67e847e7384938d345
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-08-10 15:36:41 +0200

    feat: now the send notification instruction fetch the users from the server

--- src/components/tasks-automations/SendNotificationInstructionItem.vue
+++ src/components/tasks-automations/SendNotificationInstructionItem.vue
@@ -6,11 +6,22 @@
     :instruction="props.instruction"
   >
     <p class="truncate">Send notification to</p>
-    <p v-if="!edit" class="font-bold text-center truncate">{{ instruction.email }}</p>
-    <select v-model="instruction.email" v-else class="select text-center truncate h-7 text-base-content"> <!-- Show all the users -->
-      <option :value="instruction.email" selected>{{ instruction.email }}</option>
+    <p v-if="!edit" class="font-bold text-center truncate">{{ selectedUser?.nickname }}</p>
+    <select
+      v-model="instruction.email"
+      v-else
+      class="select text-center truncate h-7 text-base-content"
+    >
+      <!-- Show all the users -->
+      <option
+        v-for="user in users"
+        :key="user.email"
+        :value="user.email"
+        :selected="instruction.email === user.email"
+      >
+        {{ user.nickname }}
+      </option>
     </select>
-    <!-- To see if it is possible to retrieve the name of a user just by using its email -->
 
     <label class="label mb-2 text-secondary-content col-span-2">Message sent</label>
     <div class="row-start-3 col-span-full">
@@ -27,7 +38,10 @@
 <script setup lang="ts">
 import type { Instruction, SendNotificationInstruction } from '@/model/scripts/Instruction'
 import InstructionLayout from './InstructionLayout.vue'
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
+import type { User } from '@/model/users-management/User'
+import { getAllUsers } from '@/api/users-management/requests/users'
+import { useUserInfoStore } from '@/stores/user-info'
 
 const props = defineProps<{
   instruction: Instruction
@@ -37,7 +51,10 @@ const props = defineProps<{
   edit: boolean
 }>()
 
+const userInfo = useUserInfoStore()
 const instruction = ref(props.instruction.instruction as SendNotificationInstruction)
+const users = ref<User[]>()
+const selectedUser = ref<User>()
 
 watch(
   () => props.instruction,
@@ -46,4 +63,9 @@ watch(
   },
   { immediate: true },
 )
+
+onMounted(async () => {
+  users.value = await getAllUsers(userInfo.token)
+  selectedUser.value = users.value?.find((u) => u.email === instruction.value.email)
+})
 </script>
