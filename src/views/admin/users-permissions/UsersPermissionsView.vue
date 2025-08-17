9cdc8e1ddcdff2eb3c9419d38686160aece98782
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-08-17 20:38:51 +0200

    feat: add loading skeletons for users permissions views

--- src/views/admin/users-permissions/UsersPermissionsView.vue
+++ src/views/admin/users-permissions/UsersPermissionsView.vue
@@ -1,9 +1,9 @@
 <template>
   <NavbarLayout title="Manage users permissions">
-    <div v-if="users.length === 0" class="flex text-center text-gray-500 justify-center items-center min-h-[50vh]">
+    <div v-if="users?.length === 0" class="flex text-center text-gray-500 justify-center items-center min-h-[50vh]">
       <p class="text-2xl">No users yet...</p>
     </div>
-    <ul class="list rounded-box">
+    <ul class="list rounded-box" v-if="users">
       <li class="list-row" v-for="user in users" :key="user.email">
         <div class="list-col-grow flex items-center">
           {{ user.nickname }}
@@ -18,6 +18,7 @@
         </a>
       </li>
     </ul>
+    <UsersListSkeleton v-else />
   </NavbarLayout>
 </template>
 
@@ -28,8 +29,9 @@ import { useUserInfoStore } from '@/stores/user-info';
 import { Role, type User } from '@/model/users-management/User';
 import * as api from '@/api/users-management/requests/users';
 import NavbarLayout from '@/components/NavbarLayout.vue';
+import UsersListSkeleton from '@/components/admin/users-permissions/UsersListSkeleton.vue'
 
-const users = ref<User[]>([]);
+const users = ref<User[] | undefined>(undefined);
 
 const loadUsers = async () => {
   const userInfoStore = useUserInfoStore();
