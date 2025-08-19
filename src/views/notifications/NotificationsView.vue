6895c8ac479df49eba5ae58db4deea6d7c13c68c
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-08-19 11:15:52 +0200

    feat: notifications are automatically read when in viewport

--- src/views/notifications/NotificationsView.vue
+++ src/views/notifications/NotificationsView.vue
@@ -1,24 +1,55 @@
 <script setup lang="ts">
 import NavbarLayout from '@/components/NavbarLayout.vue'
 import { useNotificationsStore } from '@/stores/notifications'
+import { onMounted, ref } from 'vue'
 
 const notificationsStore = useNotificationsStore()
 
 const notifications = notificationsStore.notifications
+
+const notificationRefs = ref<Element[]>([])
+
+let observer: IntersectionObserver
+
+onMounted(() => {
+  observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = entry.target.getAttribute('data-index')
+          const idx = Number(index)
+          if (Number.isFinite(idx)) {
+            const notification = notifications[idx]
+            if (!notification.read) {
+              notificationsStore.setNotificationRead(idx, true)
+              observer.unobserve(entry.target)
+            }
+          }
+        }
+      })
+    },
+    {
+      threshold: 0.5, //50% of the element must be visible to be intersecting the viewport
+    },
+  )
+
+  notificationRefs.value.forEach((el) => {
+    if (el) observer.observe(el)
+  })
+})
 </script>
 
 <template>
   <NavbarLayout title="Notifications" :show-back-button="false">
     <ul class="list rounded-box" v-if="notifications">
-      <li class="list-row" v-for="(notification, idx) in notifications" :key="idx">
+      <li
+        class="list-row"
+        v-for="(notification, idx) in notifications"
+        :key="idx"
+        :data-index="idx"
+        :ref="(el) => notificationRefs.push(el as Element)"
+      >
         <span class="list-col-grow">{{ notification.message }}</span>
-
-        <button
-          type="button"
-          @click="notificationsStore.setNotificationRead(idx, !notification.read)"
-          class="btn btn-circle btn-ghost fa-solid fa-lg !flex"
-          :class="notification.read ? 'fa-envelope-open' : 'fa-envelope'"
-        ></button>
 
         <button
           type="button"
