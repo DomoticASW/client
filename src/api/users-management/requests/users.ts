db430f33d0dc3e334c6686af5787aea27fd23b37
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-08-23 15:37:19 +0200

    refactor: moved async methods insive view page in api

--- src/api/users-management/requests/users.ts
+++ src/api/users-management/requests/users.ts
@@ -1,38 +1,62 @@
 import * as api from '@/api/api';
-import type { CompleteRegistrationRequest } from '@/model/users-management/RegistrationRequest';
+import type { CompleteRegistrationRequest, RegistrationRequest } from '@/model/users-management/RegistrationRequest';
 import type { User } from '@/model/users-management/User';
 import { tokenDeserializer } from '../dtos/GetTokenDTO';
-import { userDeserializer } from '../dtos/GetUserDTO';
+import { userDeserializer, usersDeserializer } from '../dtos/GetUserDTO';
 import { arrayDeserializer } from '@/api/Deserializer';
+import { registrationRequestDeserializer } from '../dtos/GetRegistrationRequestDTO';
 
 export async function userRegistrationRequest(registrationRequest: CompleteRegistrationRequest): Promise<void> {
-    await api.request('/api/registrationRequests', {
-      method: 'POST',
-      body: JSON.stringify(registrationRequest)
-    });
+  await api.request('/api/registrationRequests', {
+    method: 'POST',
+    body: JSON.stringify(registrationRequest)
+  });
 }
 
 export async function updateUser(token: string, body: Record<string, string>): Promise<void> {
-    await api.authorizedRequest('/api/users', token, {
-        method: 'PATCH',
-        body: JSON.stringify(body),
-    });
+  await api.authorizedRequest('/api/users', token, {
+    method: 'PATCH',
+    body: JSON.stringify(body),
+  });
+}
+
+export async function deleteUser(token: string, email: string): Promise<void> {
+  await api.authorizedRequest(`/api/users/${email}`, token, {
+    method: 'DELETE',
+  })
+}
+
+export async function rejectRegistrationRequest(token: string, email: string): Promise<void> {
+  await api.authorizedRequest(`/api/registrationRequests/${email}/reject`, token, {
+    method: 'POST',
+  })
+}
+
+export async function approveRegistrationRequest(token: string, email: string): Promise<void> {
+  await api.authorizedRequest(`/api/registrationRequests/${email}/approve`, token, {
+    method: 'POST',
+  })
 }
 
 export async function login(email: string, password: string): Promise<string> {
-    const res = await api.request('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password })
-    });
-    return await api.deserializeBody(res, tokenDeserializer);
+  const res = await api.request('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({ email, password })
+  });
+  return await api.deserializeBody(res, tokenDeserializer);
+}
+
+export async function getAllRegistrationRequests(token: string): Promise<RegistrationRequest[]> {
+  const res = await api.authorizedRequest(`/api/registrationRequests`, token)
+  return await api.deserializeBody(res, arrayDeserializer(registrationRequestDeserializer))
 }
 
 export async function getUser(token: string): Promise<User> {
-    const res = await api.authorizedRequest('/api/user', token);
-    return await api.deserializeBody(res, userDeserializer);
+  const res = await api.authorizedRequest('/api/user', token);
+  return await api.deserializeBody(res, userDeserializer);
 }
 
 export async function getAllUsers(token: string): Promise<User[]> {
-    const res = await api.authorizedRequest('/api/users', token);
-    return await api.deserializeBody(res, arrayDeserializer(userDeserializer));
+  const res = await api.authorizedRequest('/api/users', token);
+  return await api.deserializeBody(res, usersDeserializer);
 }
--- src/views/admin/ManageUsersView.vue
+++ src/views/admin/ManageUsersView.vue
@@ -1,151 +1,133 @@
 <script setup lang="ts">
-import NavbarLayout from '@/components/NavbarLayout.vue';
-import { authorizedRequest, deserializeBody } from '@/api/api'
-import { arrayDeserializer } from '@/api/Deserializer'
-import { registrationRequestDeserializer } from '@/api/users-management/dtos/GetRegistrationRequestDTO'
-import { usersDeserializer } from '@/api/users-management/dtos/GetUserDTO'
+import NavbarLayout from '@/components/NavbarLayout.vue'
 import type { RegistrationRequest } from '@/model/users-management/RegistrationRequest'
 import { Role, type User } from '@/model/users-management/User'
 import { useLoadingOverlayStore } from '@/stores/loading-overlay'
 import { presentSuccess, useSuccessPresenterStore } from '@/stores/success-presenter'
 import { useUserInfoStore } from '@/stores/user-info'
 import { onMounted, ref } from 'vue'
+import {
+  approveRegistrationRequest,
+  rejectRegistrationRequest,
+  deleteUser,
+  getAllRegistrationRequests,
+  getAllUsers,
+} from '@/api/users-management/requests/users'
 
 const loadingOverlay = useLoadingOverlayStore()
-const successPresenter = useSuccessPresenterStore();
+const successPresenter = useSuccessPresenterStore()
 const userInfo = useUserInfoStore()
 const registeredUsers = ref<User[]>()
 const unregisteredUsers = ref<RegistrationRequest[]>()
-onMounted(async () => {
-  const res = await authorizedRequest(`/api/users`, userInfo.token)
-  registeredUsers.value = await deserializeBody(res, usersDeserializer)
-})
-onMounted(async () => {
-  const res = await authorizedRequest(`/api/registrationRequests`, userInfo.token)
-  unregisteredUsers.value = await deserializeBody(res, arrayDeserializer(registrationRequestDeserializer))
-})
 
-function removeUser(user: User) {
+onMounted(async () => {
   try {
     loadingOverlay.startLoading()
-    authorizedRequest(`/api/users/${user.email}`, userInfo.token, {
-      method: 'DELETE',
-    })
-      .then(() => {
-        registeredUsers.value = registeredUsers.value?.filter((u) => u.email !== user.email)
-        showToastMessage(`Request for ${user.nickname} removed successfully.`)
-      })
-      .catch((error) => {
-        console.error('Error removing request:', error)
-      })
+    registeredUsers.value = await getAllUsers(userInfo.token)
+    unregisteredUsers.value = await getAllRegistrationRequests(userInfo.token)
+  } finally {
+    loadingOverlay.stopLoading()
+  }
+})
+
+async function removeUser(user: User) {
+  try {
+    loadingOverlay.startLoading()
+    await deleteUser(userInfo.token, user.email)
+    registeredUsers.value = registeredUsers.value?.filter((u) => u.email !== user.email)
+    showToastMessage(`Request for ${user.nickname} removed successfully.`)
   } finally {
     loadingOverlay.stopLoading()
   }
 }
 
-function rejectRequest(user: RegistrationRequest) {
+async function rejectRequest(user: RegistrationRequest) {
   try {
-      authorizedRequest(`/api/registrationRequests/${user.email}/reject`, userInfo.token, {
-      method: 'POST',
-    })
-      .then(() => {
-        unregisteredUsers.value = unregisteredUsers.value?.filter((u) => u.email !== user.email)
-        showToastMessage(`Request for ${user.nickname} rejected successfully.`)
-      })
-      .catch((error) => {
-        console.error('Error rejecting request:', error)
-      })
+    await rejectRegistrationRequest(userInfo.token, user.email)
+    unregisteredUsers.value = unregisteredUsers.value?.filter((u) => u.email !== user.email)
+    showToastMessage(`Request for ${user.nickname} rejected successfully.`)
   } finally {
     loadingOverlay.stopLoading()
   }
 }
 
-function approveRequest(user: RegistrationRequest) {
+async function approveRequest(user: RegistrationRequest) {
   try {
     loadingOverlay.startLoading()
-    authorizedRequest(`/api/registrationRequests/${user.email}/approve`, userInfo.token, {
-      method: 'POST',
-    })
-      .then(() => {
-        unregisteredUsers.value = unregisteredUsers.value?.filter((u) => u.email !== user.email)
-        const newUser: User = {
-          email: user.email,
-          nickname: user.nickname,
-          role: Role.User,
-        }
-        registeredUsers.value?.push(newUser)
-        showToastMessage(`Request for ${user.nickname} accepted successfully.`)
-      })
-      .catch((error) => {
-        console.error('Error approving request:', error)
-      })
+    await approveRegistrationRequest(userInfo.token, user.email)
+
+    unregisteredUsers.value = unregisteredUsers.value?.filter((u) => u.email !== user.email)
+    const newUser: User = {
+      email: user.email,
+      nickname: user.nickname,
+      role: Role.User,
+    }
+    registeredUsers.value?.push(newUser)
+    showToastMessage(`Request for ${user.nickname} accepted successfully.`)
   } finally {
     loadingOverlay.stopLoading()
   }
 }
 
 function showToastMessage(msg: string) {
-  successPresenter.showSuccess(presentSuccess(msg, "", 5000))
+  successPresenter.showSuccess(presentSuccess(msg, '', 5000))
 }
 </script>
 
 <template>
   <NavbarLayout title="Users permissions">
-  <div>
     <div>
       <div>
-        <ul class="list rounded-box">
-          <li class="list-row" v-for="user in registeredUsers" :key="user.email">
-            <div class="list-col-grow flex flex-col">
-              <div>{{ user.nickname }}</div>
-              <small class="text-gray-400 ms-1">{{ user.email }}</small>
-            </div>
-            <small v-if="user.role == Role.Admin" class="text-gray-400 flex items-center">{{
-              user.role
-            }}</small>
-            <button
-              v-else
-              class="btn btn-circle btn-ghost fa-solid fa-xmark"
-              type="button"
-              :aria-label="'Remove: ' + user"
-              @click="removeUser(user)"
-            >
-            </button>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <hr class="my-4 border-gray-300" />
-      <h1 class="text-2xl dark:text-white text-center">Registration requests</h1>
-      <div>
-        <ul class="list rounded-box">
-          <li class="list-row" v-for="user in unregisteredUsers" :key="user.email">
-            <div class="list-col-grow flex flex-col">
-              <div>{{ user.nickname }}</div>
-              <small class="text-gray-400 ms-1">{{ user.email }}</small>
-            </div>
-            <div>
+        <div>
+          <ul class="list rounded-box">
+            <li class="list-row" v-for="user in registeredUsers" :key="user.email">
+              <div class="list-col-grow flex flex-col">
+                <div>{{ user.nickname }}</div>
+                <small class="text-gray-400 ms-1">{{ user.email }}</small>
+              </div>
+              <small v-if="user.role == Role.Admin" class="text-gray-400 flex items-center">{{
+                user.role
+              }}</small>
               <button
+                v-else
                 class="btn btn-circle btn-ghost fa-solid fa-xmark"
                 type="button"
-                :aria-label="'Remove request of: ' + user"
-                @click="rejectRequest(user)"
-              >
-              </button>
-              <button
-                class="btn btn-circle btn-ghost fa-solid fa-plus"
-                type="button"
-                :aria-label="'Accept request of: ' + user"
-                @click="approveRequest(user)"
-              >
-              </button>
-            </div>
-          </li>
-        </ul>
+                :aria-label="'Remove: ' + user"
+                @click="removeUser(user)"
+              ></button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr class="my-4 border-gray-300" />
+        <h1 class="text-2xl dark:text-white text-center">Registration requests</h1>
+        <div>
+          <ul class="list rounded-box">
+            <li class="list-row" v-for="user in unregisteredUsers" :key="user.email">
+              <div class="list-col-grow flex flex-col">
+                <div>{{ user.nickname }}</div>
+                <small class="text-gray-400 ms-1">{{ user.email }}</small>
+              </div>
+              <div>
+                <button
+                  class="btn btn-circle btn-ghost fa-solid fa-xmark"
+                  type="button"
+                  :aria-label="'Remove request of: ' + user"
+                  @click="rejectRequest(user)"
+                ></button>
+                <button
+                  class="btn btn-circle btn-ghost fa-solid fa-plus"
+                  type="button"
+                  :aria-label="'Accept request of: ' + user"
+                  @click="approveRequest(user)"
+                ></button>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
   </NavbarLayout>
 </template>
 
