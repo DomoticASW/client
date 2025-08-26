f3a1aff54afb156ead40bbfdc53f6384d770449f
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-08-26 15:03:20 +0200

    refactor: move update of group store before of app.mount(#app)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { useUserInfoStore } from './stores/user-info'
-import { isGetUserInfoDTO } from '@/api/users-management/dtos/GetUserInfoDTO'
 import { useErrorPresenterStore } from './stores/error-presenter'
 import { useNotificationsStore } from './stores/notifications'
 import { useGroupsStore } from './stores/groups'
@@ -42,25 +41,6 @@ app.config.errorHandler = (err) => {
   }
 }
 
-// During development it's possible to set a VITE_USER_INFO object to skip login:
-// VITE_USER_INFO='{"email": "a@email.com", "nickname": "Foo", "token": "blablabla", "role": "Admin" }' npm run dev
-const userInfoStr = import.meta.env.VITE_USER_INFO
-if (import.meta.env.DEV && userInfoStr) {
-  try {
-    const userInfo = JSON.parse(userInfoStr)
-    if (!isGetUserInfoDTO(userInfo)) {
-      throw new Error()
-    } else {
-      const userInfoStore = useUserInfoStore()
-      userInfoStore.userInfo = userInfo
-    }
-  } catch (e) {
-    throw new Error("Value of VITE_USER_INFO is not valid.\n" + (e as Error).message)
-  }
-}
-
-app.mount('#app')
-
 if (useUserInfoStore().token) {
   try {
     loadingOverlay.startLoading()
@@ -69,3 +49,5 @@ if (useUserInfoStore().token) {
     loadingOverlay.stopLoading()
   }
 }
+
+app.mount('#app')
