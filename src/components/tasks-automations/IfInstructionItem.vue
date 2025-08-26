529b350ba5f4f77104ab244366626c89acee1983
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-08-26 19:19:23 +0200

    chore: invert not with operator on ifs

--- src/components/tasks-automations/IfInstructionItem.vue
+++ src/components/tasks-automations/IfInstructionItem.vue
@@ -17,21 +17,11 @@
       :class="depth > 1 ? 'w-full col-span-3' : 'w-17 md:w-25 col-span-3'"
       v-model="instruction.condition.leftConstantName"
     />
-    <select
-      v-if="edit"
-      v-model="instruction.condition.conditionOperatorType"
-      class="select select-primary w-17 md:w-20 place-self-center h-7"
-      :class="depth > 1 ? 'row-start-2 col-start-2 ml-2' : 'col-span-3'"
-    >
-      <option v-for="operator in ConditionOperatorType" :key="operator" :value="operator">
-        {{ getOperator(operator) }}
-      </option>
-    </select>
     <label
       v-if="edit"
       for="negate"
-      class="label text-sm md:text-base text-base-content place-self-center col-span-2"
-      :class="depth > 1 ? 'row-start-2 ml-4' : ''"
+      class="label text-sm md:text-base text-base-content place-self-center"
+      :class="depth > 1 ? 'row-start-2 col-start-2 mr-3' : 'col-span-2'"
     >
       Not
       <input
@@ -42,11 +32,23 @@
         v-model="instruction.condition.negate"
       />
     </label>
+    <select
+      v-if="edit"
+      v-model="instruction.condition.conditionOperatorType"
+      class="select select-primary w-17 md:w-20 place-self-center h-7"
+      :class="depth > 1 ? 'row-start-2 col-start-3 ml-1' : 'col-span-3'"
+    >
+      <option v-for="operator in ConditionOperatorType" :key="operator" :value="operator">
+        {{ getOperator(operator) }}
+      </option>
+    </select>
     <input
       v-if="edit"
       type="text"
       class="input input-primary place-self-center h-7 text-center"
-      :class="depth > 1 ? 'row-start-3 col-start-2 col-span-full w-full' : 'col-span-3 w-17 md:w-25'"
+      :class="
+        depth > 1 ? 'row-start-3 col-start-2 col-span-3 w-full' : 'col-span-3 w-17 md:w-25'
+      "
       placeholder="Constant name"
       v-model="instruction.condition.rightConstantName"
     />
